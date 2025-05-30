761586f78e184fd287f2895c262922f0a8fdf71f
bharath <blbreddy0005@gmail.com>
2025-05-30 18:28:41 +0530

    PwdLogin

--- DayEaseUserApp/DayEaseUserApp/Program.cs
+++ DayEaseUserApp/DayEaseUserApp/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddScoped(sp => new HttpClient
 
 builder.Services.AddScoped<IApiService, ApiService>();
 builder.Services.AddScoped<IRegistration, RegistrationService>();
+builder.Services.AddScoped<IUserLoginService, UserLoginService>();
 //builder.Services.AddScoped<RegistrationState>();
 builder.Services.AddBlazoredLocalStorage();
 // ✅ Use Blazored Session Storage for session-like behavior in Blazor WASM
@@ -35,27 +36,4 @@ await builder.Build().RunAsync();
 
 
 
-//var builder = WebAssemblyHostBuilder.CreateDefault(args);
-//builder.RootComponents.Add<App>("#app");
-//builder.RootComponents.Add<HeadOutlet>("head::after");
-//// Add configuration for ApiSettings
-//builder.Services.Configure<ApiSettings>(builder.Configuration.GetSection("ApiSettings"));
-////builder.Services.AddHttpClient<IRegistration, RegistrationService>();
-////builder.Services.AddScoped<IRegistration, RegistrationService>();
-//builder.Services.AddHttpClient<IRegistration, RegistrationService>(client =>
-//{
-//    client.BaseAddress = new Uri(builder.Configuration["ApiSettings:DayEase_API"]);
-//});
 
-//builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-////var config = builder.Configuration.GetSection("ApiSettings")["DayEase_API"];
-////Console.WriteLine("API URL from config: " + config);
-
-//// ? Register ApiService
-//builder.Services.AddScoped<ApiService>();
-//builder.Services.AddScoped<UserService>();
-
-
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-//await builder.Build().RunAsync();
---
+++ DayEaseUserApp/Domain/RequestModels/UserLoginRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.RequestModels
+{
+    public class UserLoginRequest
+    {
+
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Username must be exactly 10 characters.")]
+        public string phonenumber { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string password { get; set; }
+
+
+    }
+}
