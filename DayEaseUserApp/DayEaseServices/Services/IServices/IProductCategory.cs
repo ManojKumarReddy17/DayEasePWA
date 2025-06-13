9bf9a3faadc2e55f2e0a06797d06ac42554592f4
Venkatasaikanala <kanalavenkatasai@gmail.com>
2025-06-13 17:58:56 +0530

    added product category

---
+++ DayEaseUserApp/DayEaseServices/Services/IServices/IProductCategory.cs
@@ -0,0 +1,22 @@
+﻿using Domain.RequestModels;
+using Domain.ResponseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayEaseServices.Services.IServices
+{
+      public  interface IProductCategory
+      {
+        Task<List<CategoryRequest>> GetProductcategoByStoreId(CategoryRequest model);
+
+        Task<MysqlResponse<Pagination<ProductRequestModel>>> GetProductsByStoreId(PaginationQueryInput model);
+
+        //Task<List<ProductCategoriesModel>> GetProductsByStoreId(StoreProdCategoryRequestModel model);
+        
+
+
+      }
+}
---
+++ DayEaseUserApp/Domain/RequestModels/Pagination.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.RequestModels
+{
+   public class Pagination<T>
+    {
+      
+            public int? pageNumber { get; set; }
+            public int? pageSize { get; set; }
+            public int TotalItems { set; get; }
+            public int TotalPageCount { set; get; }
+            public List<T> Items { get; set; }
+        }
+        public class PaginationInput
+        {
+            public string StoreId { get; set; }
+            public int? PageNumber { get; set; }
+            public int? PageSize { get; set; }
+
+        }
+    public class PaginationQueryInput
+    {
+        public ProductRequestModel Product { get; set; }
+        public PaginationInput PaginationInput { get; set; } = new PaginationInput();
+    }
+
+
+
+}
