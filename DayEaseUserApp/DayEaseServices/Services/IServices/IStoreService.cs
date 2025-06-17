bdc7474cc9580aef4776e6e5e25fb5640c6e16f5
Tharun <tharunkumar2904@gmail.com>
2025-06-17 17:49:59 +0530

    Modify the Login and Higlight pages

---
+++ DayEaseUserApp/DayEaseServices/Services/IServices/IStoreService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.DomainModels;
+using Domain.RequestModels;
+
+namespace DayEaseServices.Services.IServices
+{
+    
+        public interface IStoreService
+        {
+            Task<NearbyStoresResponse> GetNearbyStoresAsync(NearbyStoresRequest request);
+        }
+
+    
+}
