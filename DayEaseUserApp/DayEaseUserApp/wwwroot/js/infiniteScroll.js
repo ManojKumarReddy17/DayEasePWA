5c69e9e88ec0e3b483dc776e4bda84efe5d58cdf
Venkatasaikanala <kanalavenkatasai@gmail.com>
2025-06-26 13:12:07 +0530

    ProductCategory_page,added infinitescroll.js

---
+++ DayEaseUserApp/DayEaseUserApp/wwwroot/js/infiniteScroll.js
@@ -0,0 +1,16 @@
+﻿window.setupInfiniteScroll = (sentinel, dotNetHelper) => {
+    if (!(sentinel instanceof Element)) {
+        console.error("Sentinel is not a valid DOM element");
+        return;
+    }
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                dotNetHelper.invokeMethodAsync("OnScrollNearBottom");
+            }
+        });
+    });
+
+    observer.observe(sentinel);
+};
