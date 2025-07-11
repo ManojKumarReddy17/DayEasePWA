af289af91961066fe1b28f5fa83d49c4969a6dec
diya <diya@gmail.com>
2025-07-11 11:37:46 +0530

    Updated user profile and wallet

---
+++ DayEaseUserApp/Domain/DomainModels/UserResponseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DomainModels
+{
+    public class UserResponseModel
+    {
+        public bool Success { get; set; }           // Indicates success or failure
+        public string Message { get; set; }
+
+        public string RegistrationStatus { get; set; } // Message for success or error
+        public int StatusCode { get; internal set; }
+    }
+}
