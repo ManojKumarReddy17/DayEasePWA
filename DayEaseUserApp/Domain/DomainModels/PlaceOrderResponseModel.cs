a1f4d9c251d4d615840f0464dd8adfc42170fd5d
rakesh sangala <rakeshsangala7@gmail.com>
2025-07-23 16:32:08 +0530

    Create Order

---
+++ DayEaseUserApp/Domain/DomainModels/PlaceOrderResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.DomainModels
+{
+    public class PlaceOrderResponseModel
+    {
+        public string OrderId { get; set; }
+        public string Message { get; set; }
+        public bool Success { get; set; }
+    }
+}
