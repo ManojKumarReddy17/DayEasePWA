4c7074c29848ca9f22a46458cc9f16faa878cd59
rakesh sangala <rakeshsangala7@gmail.com>
2025-07-28 17:05:20 +0530

    checkout details

--- DayEaseUserApp/Domain/RequestModels/OrderRequestModel.cs
+++ DayEaseUserApp/Domain/RequestModels/OrderRequestModel.cs
@@ -38,6 +38,7 @@ namespace Domain.RequestModels
         public string? ProductName { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
+        public string CartItemId { get; set; }
     }
 
 
